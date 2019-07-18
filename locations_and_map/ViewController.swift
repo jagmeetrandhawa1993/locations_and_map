6491d4e908d26a2a3f113b269afb0009699fab1f
jagmeetkaur <jagmeetrandhawa1993@gmail.com>
2019-07-18 17:07:01 -0400

    Locatins

--- locations_and_map/ViewController.swift
+++ locations_and_map/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    
+    @IBOutlet weak var myTextView: UITextView!
+    
+    let locationManager = CLLocationManager()
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+
+       mapView.delegate = self
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let newLocation = locations.last {
+            let latitudeString = "\(newLocation.coordinate.latitude)"
+            let longitudeString = "\(newLocation.coordinate.longitude)"
+            myTextView.text = "Latitude: " + latitudeString + "\nLongitude: " + longitudeString
+        }
     }
 
+   func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let zoomArea = MKCoordinateRegion(center: self.mapView.userLocation.coordinate, span:
+                MKCoordinateSpan (latitudeDelta: 0.05, longitudeDelta: 0.05))
+    self.mapView.setRegion(zoomArea, animated: true)
+        }
 
+        
+        
+        
+        
 }
+
 
